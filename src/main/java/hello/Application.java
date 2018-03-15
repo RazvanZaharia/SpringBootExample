525d38d0fe6e45e4c9a575cd8d9f8a1b709bf443
Razvan <razvan@fieldmargin.com>
2018-03-15 13:32:43 +0000

    no message

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -13,13 +13,7 @@ import org.springframework.context.annotation.Bean;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
-
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
