d076cbd1d8591c6e070ec3312b5e5d9f7049df64
Razvan <razvan@fieldmargin.com>
2018-03-19 14:06:45 +0000

    5hours expiration time for jwts

--- src/main/java/hello/config/security/token/jwt/JWTConstants.java
+++ src/main/java/hello/config/security/token/jwt/JWTConstants.java
@@ -3,7 +3,7 @@ package hello.config.security.token.jwt;
 public class JWTConstants {
 
     public static final String SECRET = "SecretKeyToGenJWTs";
-    public static final long EXPIRATION_TIME = 864_000_000; // 10 days
+    public static final long EXPIRATION_TIME = 36_000_000; // 5 hours
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
 
